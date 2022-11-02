531621a06106bc034d042fd0d60842594c8c7e06
rnibhriain <78806345+rnibhriain@users.noreply.github.com>
2022-11-02 18:48:17 +0000

    Trying to Add Snow ground

--- Lab 04 - Sample Object Hierarchy(1) (1)/Lab 04 - Sample Object Hierarchy/Lab04/main.cpp
+++ Lab 04 - Sample Object Hierarchy(1) (1)/Lab 04 - Sample Object Hierarchy/Lab04/main.cpp
@@ -1,4 +1,3 @@
-
 //Some Windows Headers (For Time, IO, etc.)
 #include <windows.h>
 #include <mmsystem.h>
@@ -14,27 +13,116 @@
 #include <iostream>
 #include <sstream>
 
+// Assimp includes
+#include <assimp/cimport.h> // scene importer
+#include <assimp/scene.h> // collects data
+#include <assimp/postprocess.h> // various extra operations
 using namespace std;
-
-
 
 //typedef double DWORD;
 vec3 PosData(0.0f, 0.0f, 3.0f);
 
 Camera camera(PosData);
 
+/*----------------------------------------------------------------------------
+MESH TO LOAD
+----------------------------------------------------------------------------*/
+// this mesh is a dae file format but you should be able to use any other format too, obj is typically what is used
+// put the mesh in your project directory, or provide a filepath for it here
+#define MESH_NAME "reindeer2.dae"
+#define PLANE_MESH "snow.dae"
+/*----------------------------------------------------------------------------
+----------------------------------------------------------------------------*/
+unsigned int plane_vp_vbo = 0;
+unsigned int plane_vn_vbo = 0;
+unsigned int plane_vt_vbo = 0;
+
+
+#pragma region SimpleTypes
+typedef struct
+{
+	size_t mPointCount = 0;
+	std::vector<vec3> mVertices;
+	std::vector<vec3> mNormals;
+	std::vector<vec2> mTextureCoords;
+} ModelData;
+#pragma endregion SimpleTypes
+
+using namespace std;
+GLuint shaderProgramID;
+
+ModelData mesh_data;
+ModelData plane_data;
+unsigned int mesh_vao = 0;
+
 
 // Macro for indexing vertex buffer
 #define BUFFER_OFFSET(i) ((char *)NULL + (i))
 
 
-GLuint shaderProgramID;
 
 unsigned int teapot_vao = 0;
 int width = 800.0;
 int height = 600.0;
-GLuint loc1;
-GLuint loc2;
+
+#pragma region MESH LOADING
+/*----------------------------------------------------------------------------
+MESH LOADING FUNCTION
+----------------------------------------------------------------------------*/
+
+ModelData load_mesh(const char* file_name) {
+	ModelData modelData;
+
+	/* Use assimp to read the model file, forcing it to be read as    */
+	/* triangles. The second flag (aiProcess_PreTransformVertices) is */
+	/* relevant if there are multiple meshes in the model file that   */
+	/* are offset from the origin. This is pre-transform them so      */
+	/* they're in the right position.                                 */
+	const aiScene* scene = aiImportFile(
+		file_name,
+		aiProcess_Triangulate | aiProcess_PreTransformVertices
+	);
+
+	if (!scene) {
+		fprintf(stderr, "ERROR: reading mesh %s\n", file_name);
+		return modelData;
+	}
+
+	printf("  %i materials\n", scene->mNumMaterials);
+	printf("  %i meshes\n", scene->mNumMeshes);
+	printf("  %i textures\n", scene->mNumTextures);
+
+	for (unsigned int m_i = 0; m_i < scene->mNumMeshes; m_i++) {
+		const aiMesh* mesh = scene->mMeshes[m_i];
+		printf("    %i vertices in mesh\n", mesh->mNumVertices);
+		modelData.mPointCount += mesh->mNumVertices;
+		for (unsigned int v_i = 0; v_i < mesh->mNumVertices; v_i++) {
+			if (mesh->HasPositions()) {
+				const aiVector3D* vp = &(mesh->mVertices[v_i]);
+				modelData.mVertices.push_back(vec3(vp->x, vp->y, vp->z));
+			}
+			if (mesh->HasNormals()) {
+				const aiVector3D* vn = &(mesh->mNormals[v_i]);
+				modelData.mNormals.push_back(vec3(vn->x, vn->y, vn->z));
+			}
+			if (mesh->HasTextureCoords(0)) {
+				const aiVector3D* vt = &(mesh->mTextureCoords[0][v_i]);
+				modelData.mTextureCoords.push_back(vec2(vt->x, vt->y));
+			}
+			if (mesh->HasTangentsAndBitangents()) {
+				/* You can extract tangents and bitangents here              */
+				/* Note that you might need to make Assimp generate this     */
+				/* data for you. Take a look at the flags that aiImportFile  */
+				/* can take.                                                 */
+			}
+		}
+	}
+
+	aiReleaseImport(scene);
+	return modelData;
+}
+
+#pragma endregion MESH LOADING
 
 
 float deltaTime = 0.0f;	// Time between current frame and last frame
@@ -43,99 +131,178 @@ float lastFrame = 0.0f; // Time of last frame
 // Shader Functions- click on + to expand
 #pragma region SHADER_FUNCTIONS
 
-std::string readShaderSource(const std::string& fileName)
-{
-	std::ifstream file(fileName.c_str()); 
-	if(file.fail()) {
-		cout << "error loading shader called " << fileName;
-		exit (1); 
-	} 
-	
-	std::stringstream stream;
-	stream << file.rdbuf();
-	file.close();
+char* readShaderSource(const char* shaderFile) {
+	FILE* fp;
+	fopen_s(&fp, shaderFile, "rb");
 
-	return stream.str();
+	if (fp == NULL) { return NULL; }
+
+	fseek(fp, 0L, SEEK_END);
+	long size = ftell(fp);
+
+	fseek(fp, 0L, SEEK_SET);
+	char* buf = new char[size + 1];
+	fread(buf, 1, size, fp);
+	buf[size] = '\0';
+
+	fclose(fp);
+
+	return buf;
 }
+
 
 static void AddShader(GLuint ShaderProgram, const char* pShaderText, GLenum ShaderType)
 {
 	// create a shader object
-    GLuint ShaderObj = glCreateShader(ShaderType);
+	GLuint ShaderObj = glCreateShader(ShaderType);
 
-    if (ShaderObj == 0) {
-        fprintf(stderr, "Error creating shader type %d\n", ShaderType);
-        exit(0);
-    }
-	std::string outShader = readShaderSource(pShaderText);
-	const char* pShaderSource = outShader.c_str();
+	if (ShaderObj == 0) {
+		std::cerr << "Error creating shader..." << std::endl;
+		std::cerr << "Press enter/return to exit..." << std::endl;
+		std::cin.get();
+		exit(1);
+	}
+	const char* pShaderSource = readShaderSource(pShaderText);
 
 	// Bind the source code to the shader, this happens before compilation
 	glShaderSource(ShaderObj, 1, (const GLchar**)&pShaderSource, NULL);
 	// compile the shader and check for errors
-    glCompileShader(ShaderObj);
-    GLint success;
+	glCompileShader(ShaderObj);
+	GLint success;
 	// check for shader related errors using glGetShaderiv
-    glGetShaderiv(ShaderObj, GL_COMPILE_STATUS, &success);
-    if (!success) {
-        GLchar InfoLog[1024];
-        glGetShaderInfoLog(ShaderObj, 1024, NULL, InfoLog);
-        fprintf(stderr, "Error compiling shader type %d: '%s'\n", ShaderType, InfoLog);
-        exit(1);
-    }
+	glGetShaderiv(ShaderObj, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		GLchar InfoLog[1024] = { '\0' };
+		glGetShaderInfoLog(ShaderObj, 1024, NULL, InfoLog);
+		std::cerr << "Error compiling "
+			<< (ShaderType == GL_VERTEX_SHADER ? "vertex" : "fragment")
+			<< " shader program: " << InfoLog << std::endl;
+		std::cerr << "Press enter/return to exit..." << std::endl;
+		std::cin.get();
+		exit(1);
+	}
 	// Attach the compiled shader object to the program object
-    glAttachShader(ShaderProgram, ShaderObj);
+	glAttachShader(ShaderProgram, ShaderObj);
 }
 
 GLuint CompileShaders()
 {
 	//Start the process of setting up our shaders by creating a program ID
 	//Note: we will link all the shaders together into this ID
-    shaderProgramID = glCreateProgram();
-    if (shaderProgramID == 0) {
-        fprintf(stderr, "Error creating shader program\n");
-        exit(1);
-    }
+	shaderProgramID = glCreateProgram();
+	if (shaderProgramID == 0) {
+		std::cerr << "Error creating shader program..." << std::endl;
+		std::cerr << "Press enter/return to exit..." << std::endl;
+		std::cin.get();
+		exit(1);
+	}
 
 	// Create two shader objects, one for the vertex, and one for the fragment shader
-    AddShader(shaderProgramID, "C:/Users/User/Downloads/Lab 04 - Sample Object Hierarchy(1) (1)/Lab 04 - Sample Object Hierarchy/Shaders/simpleVertexShader.txt", GL_VERTEX_SHADER);
-    AddShader(shaderProgramID, "C:/Users/User/Downloads/Lab 04 - Sample Object Hierarchy(1) (1)/Lab 04 - Sample Object Hierarchy/Shaders/simpleFragmentShader.txt", GL_FRAGMENT_SHADER);
+	AddShader(shaderProgramID, "C:/Users/User/Documents/computer-graphics/Lab 04 - Sample Object Hierarchy(1) (1)/Lab 04 - Sample Object Hierarchy/Shaders/simpleVertexShader.txt", GL_VERTEX_SHADER);
+	AddShader(shaderProgramID, "C:/Users/User/Documents/computer-graphics/Lab 04 - Sample Object Hierarchy(1) (1)/Lab 04 - Sample Object Hierarchy/Shaders/simpleFragmentShader.txt", GL_FRAGMENT_SHADER);
 
-    GLint Success = 0;
-    GLchar ErrorLog[1024] = { 0 };
-
-
+	GLint Success = 0;
+	GLchar ErrorLog[1024] = { '\0' };
 	// After compiling all shader objects and attaching them to the program, we can finally link it
-    glLinkProgram(shaderProgramID);
+	glLinkProgram(shaderProgramID);
 	// check for program related errors using glGetProgramiv
-    glGetProgramiv(shaderProgramID, GL_LINK_STATUS, &Success);
+	glGetProgramiv(shaderProgramID, GL_LINK_STATUS, &Success);
 	if (Success == 0) {
 		glGetProgramInfoLog(shaderProgramID, sizeof(ErrorLog), NULL, ErrorLog);
-		fprintf(stderr, "Error linking shader program: '%s'\n", ErrorLog);
-        exit(1);
+		std::cerr << "Error linking shader program: " << ErrorLog << std::endl;
+		std::cerr << "Press enter/return to exit..." << std::endl;
+		std::cin.get();
+		exit(1);
 	}
 
 	// program has been successfully linked but needs to be validated to check whether the program can execute given the current pipeline state
-    glValidateProgram(shaderProgramID);
+	glValidateProgram(shaderProgramID);
 	// check for program related errors using glGetProgramiv
-    glGetProgramiv(shaderProgramID, GL_VALIDATE_STATUS, &Success);
-    if (!Success) {
-        glGetProgramInfoLog(shaderProgramID, sizeof(ErrorLog), NULL, ErrorLog);
-        fprintf(stderr, "Invalid shader program: '%s'\n", ErrorLog);
-        exit(1);
-    }
+	glGetProgramiv(shaderProgramID, GL_VALIDATE_STATUS, &Success);
+	if (!Success) {
+		glGetProgramInfoLog(shaderProgramID, sizeof(ErrorLog), NULL, ErrorLog);
+		std::cerr << "Invalid shader program: " << ErrorLog << std::endl;
+		std::cerr << "Press enter/return to exit..." << std::endl;
+		std::cin.get();
+		exit(1);
+	}
 	// Finally, use the linked shader program
 	// Note: this program will stay in effect for all draw calls until you replace it with another or explicitly disable its use
-    glUseProgram(shaderProgramID);
-
+	glUseProgram(shaderProgramID);
 	return shaderProgramID;
 }
 #pragma endregion SHADER_FUNCTIONS
 
+GLuint loc1, loc2, loc3;
+
 // VBO Functions - click on + to expand
 #pragma region VBO_FUNCTIONS
 
+void generateObjectBufferMesh() {
+	/*----------------------------------------------------------------------------
+	LOAD MESH HERE AND COPY INTO BUFFERS
+	----------------------------------------------------------------------------*/
 
+	//Note: you may get an error "vector subscript out of range" if you are using this code for a mesh that doesnt have positions and normals
+	//Might be an idea to do a check for that before generating and binding the buffer.
+
+	mesh_data = load_mesh(MESH_NAME);
+
+	plane_data = load_mesh(PLANE_MESH);
+
+	plane_vp_vbo = 0;
+	glGenBuffers(1, &plane_vp_vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, plane_vp_vbo);
+	glBufferData(GL_ARRAY_BUFFER, plane_data.mPointCount * sizeof(vec3), &plane_data.mVertices[0], GL_STATIC_DRAW);
+
+	plane_vn_vbo = 0;
+	glGenBuffers(1, &plane_vn_vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, plane_vn_vbo);
+	glBufferData(GL_ARRAY_BUFFER, plane_data.mPointCount * sizeof(vec3), &plane_data.mNormals[0], GL_STATIC_DRAW);
+
+	plane_vt_vbo = 0;
+	glGenBuffers(1, &plane_vt_vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, plane_vt_vbo);
+	glBufferData(GL_ARRAY_BUFFER, plane_data.mPointCount * sizeof(vec2), &plane_data.mTextureCoords[0], GL_STATIC_DRAW);
+
+	unsigned int vp_vbo = 0;
+	loc1 = glGetAttribLocation(shaderProgramID, "vertex_position");
+	loc2 = glGetAttribLocation(shaderProgramID, "vertex_normal");
+	loc3 = glGetAttribLocation(shaderProgramID, "vertex_texture");
+
+	glGenBuffers(1, &vp_vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vp_vbo);
+	glBufferData(GL_ARRAY_BUFFER, mesh_data.mPointCount * sizeof(vec3), &mesh_data.mVertices[0], GL_STATIC_DRAW);
+	unsigned int vn_vbo = 0;
+
+	/*
+	glGenBuffers(1, &vn_vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vn_vbo);
+	glBufferData(GL_ARRAY_BUFFER, mesh_data.mPointCount * sizeof(vec3), &mesh_data.mNormals[0], GL_STATIC_DRAW);
+	*/
+	//	This is for texture coordinates which you don't currently need, so I have commented it out
+	//	unsigned int vt_vbo = 0;
+	//	glGenBuffers (1, &vt_vbo);
+	//	glBindBuffer (GL_ARRAY_BUFFER, vt_vbo);
+	//	glBufferData (GL_ARRAY_BUFFER, monkey_head_data.mTextureCoords * sizeof (vec2), &monkey_head_data.mTextureCoords[0], GL_STATIC_DRAW);
+
+	unsigned int vao = 0;
+	glBindVertexArray(vao);
+
+	glEnableVertexAttribArray(loc1);
+	glBindBuffer(GL_ARRAY_BUFFER, vp_vbo);
+	glVertexAttribPointer(loc1, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+	/*
+	glEnableVertexAttribArray(loc2);
+	glBindBuffer(GL_ARRAY_BUFFER, vn_vbo);
+	glVertexAttribPointer(loc2, 3, GL_FLOAT, GL_FALSE, 0, NULL);*/
+
+	//	This is for texture coordinates which you don't currently need, so I have commented it out
+	//	glEnableVertexAttribArray (loc3);
+	//	glBindBuffer (GL_ARRAY_BUFFER, vt_vbo);
+	//	glVertexAttribPointer (loc3, 2, GL_FLOAT, GL_FALSE, 0, NULL);
+}
 
 
 
@@ -192,6 +359,10 @@ void display(){
 	int matrix_location = glGetUniformLocation (shaderProgramID, "model");
 	int view_mat_location = glGetUniformLocation (shaderProgramID, "view");
 	int proj_mat_location = glGetUniformLocation (shaderProgramID, "proj");
+
+	loc1 = glGetAttribLocation(shaderProgramID, "vertex_position");
+	loc2 = glGetAttribLocation(shaderProgramID, "vertex_normal");
+	loc3 = glGetAttribLocation(shaderProgramID, "vertex_texture");
 	
 
 	//Here is where the code for the viewport lab will go, to get you started I have drawn a t-pot in the bottom left
@@ -207,11 +378,43 @@ void display(){
 	mat4 persp_proj = perspective(camera.Zoom, (float)width / (float)height, 0.1f, 100.0f);
 	mat4 model = identity_mat4();
 
+
+	mat4 Plane = identity_mat4();
+	Plane = translate(Plane, vec3(5.0f, 0.0f, 0.0f));
+	glEnableVertexAttribArray(loc1);
+	glBindBuffer(GL_ARRAY_BUFFER, plane_vp_vbo);
+	glVertexAttribPointer(loc1, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+	glEnableVertexAttribArray(loc2);
+	glBindBuffer(GL_ARRAY_BUFFER, plane_vn_vbo);
+	glVertexAttribPointer(loc2, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+	glEnableVertexAttribArray(loc3);
+	glBindBuffer(GL_ARRAY_BUFFER, plane_vt_vbo);
+	glVertexAttribPointer(loc3, 2, GL_FLOAT, GL_FALSE, 0, NULL);
+	glUniformMatrix4fv(matrix_location, 1, GL_FALSE, Plane.m);
+
+	glDrawArrays(GL_TRIANGLES, 0, plane_data.mPointCount);
+
+
+	/*
+	// Set up the child matrix
+	mat4 modelChild = identity_mat4();
+	modelChild = rotate_z_deg(modelChild, 180);
+	modelChild = rotate_y_deg(modelChild, rotate_y);
+	modelChild = translate(modelChild, vec3(0.0f, 1.9f, 0.0f));
+
+	// Apply the root matrix to the child matrix
+	modelChild = model * modelChild;
+
+	// Update the appropriate unif..orm and draw the mesh again
+	glUniformMatrix4fv(matrix_location, 1, GL_FALSE, modelChild.m);*/
+
 	//glViewport (0, 0, width / 2, height / 2);
+
+
 	glUniformMatrix4fv (proj_mat_location, 1, GL_FALSE, persp_proj.m);
 	glUniformMatrix4fv (view_mat_location, 1, GL_FALSE, view.m);
 	glUniformMatrix4fv (matrix_location, 1, GL_FALSE, model.m);
-	glDrawArrays (GL_TRIANGLES, 0, teapot_vertex_count);
+	glDrawArrays (GL_TRIANGLES, 0, mesh_data.mPointCount);
 
 
     glutSwapBuffers();
@@ -292,6 +495,7 @@ void mouseCallback(int x, int y) {
 
 void init()
 {
+	/*/
 	// Create 3 vertices that make up a triangle that fits on the viewport 
 	GLfloat vertices[] = {-1.0f, -1.0f, 0.0f, 1.0,
 			1.0f, -1.0f, 0.0f, 1.0, 
@@ -299,12 +503,15 @@ void init()
 	// Create a color array that identfies the colors of each vertex (format R, G, B, A)
 	GLfloat colors[] = {0.0f, 1.0f, 0.0f, 1.0f,
 			1.0f, 0.0f, 0.0f, 1.0f,
-			0.0f, 0.0f, 1.0f, 1.0f};
+			0.0f, 0.0f, 1.0f, 1.0f};*/
 	// Set up the shaders
 	GLuint shaderProgramID = CompileShaders();
 
+
 	// load teapot mesh into a vertex buffer array
-	generateObjectBufferTeapot ();
+	//generateObjectBufferTeapot ();
+
+	generateObjectBufferMesh();
 	
 }
 
